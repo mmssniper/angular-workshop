cceef6dfee030d9a1a73bcbba18f84a5f7c10b0c
Maro Marčinko <maro.marcinko@in2.hr>
2017-04-28 14:36:47 +0200

    8-Commit Enable Cors

---
+++ 8-EnableCors/IN2.angular-workshop.server/IN2.angular-workshop.server/ModelBinders/ProductDiscountModelBinder.cs
@@ -0,0 +1,48 @@
+﻿using IN2.angular_workshop.server.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Controllers;
+using System.Web.Http.ModelBinding;
+using System.Web.Http.ValueProviders;
+
+namespace IN2.angular_workshop.server.ModelBinders
+{
+    public class ProductDiscountModelBinder : IModelBinder
+    {
+        public bool BindModel(HttpActionContext actionContext, ModelBindingContext bindingContext)
+        {
+            if (bindingContext.ModelType != typeof(ProductDiscount))
+            {
+                return false;
+            }
+
+            ValueProviderResult val = bindingContext.ValueProvider.GetValue(bindingContext.ModelName);
+
+            if (val == null)
+            {
+                return false;
+            }
+
+            string key = val.RawValue as string;
+
+            if (key == null)
+            {
+                bindingContext.ModelState.AddModelError(bindingContext.ModelName, "Wrong value type");
+                return false;
+            }
+
+            ProductDiscount result;
+
+            if (ProductDiscount.TryParse(key, out result))
+            {
+                bindingContext.Model = result;
+                return true;
+            }
+
+            bindingContext.ModelState.AddModelError(bindingContext.ModelName, "Cannot convert value to discount");
+            return false;
+        }
+    }
+}
